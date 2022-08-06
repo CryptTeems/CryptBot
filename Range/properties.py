8967e84efbe0fb92c753963503e0c2ed217edae2
shuta <sdsds.com@hotmail.co.jp>
2022-08-06 21:19:57 +0900

    make Master

--- Range/properties.py
+++ Range/properties.py
@@ -1,5 +1,5 @@
-API_KEY = '7MlQofOjfeh6l4lxtGaqLqr04IAxwimmuqYobvozpBmqipd259dZxxHs1tBVu70a'
-SECRET_KEY = 'eIIwHSRH6bpKAlAmAfhVDMjt1pmRopvSp54uC9fVF09PnYmCwS4ye2rwpsfsWOa3'
+API_KEY = '
+SECRET_KEY =
 ticker = 'MATIC'  # 取引対象
 currency = 'USDT'  # USDT建て
 symbol = ticker + currency
