6d1fc61ed4781e9fa9280bc54fa2a321636de980
shuta <sdsds.com@hotmail.co.jp>
2022-08-21 18:00:25 +0900

    make Master

--- analysis/analysis_range.py
+++ analysis/analysis_range.py
@@ -11,6 +11,13 @@ from binance.client import Client
 from binance.exceptions import BinanceAPIException
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import datetime
+
+
+"""
+全体的につくり直し
+"""
+
 
 API_KEY = '7MlQofOjfeh6l4lxtGaqLqr04IAxwimmuqYobvozpBmqipd259dZxxHs1tBVu70a'
 SECRET_KEY = 'eIIwHSRH6bpKAlAmAfhVDMjt1pmRopvSp54uC9fVF09PnYmCwS4ye2rwpsfsWOa3'
@@ -21,6 +28,8 @@ binance = Client(API_KEY, SECRET_KEY)
 
 df_chart_chart = pd.DataFrame()
 df_income_history = pd.DataFrame()
+df_income_long_history = pd.DataFrame()
+df_income_short_history = pd.DataFrame()
 
 data_chart = binance.get_historical_klines("MATICUSDT", Client.KLINE_INTERVAL_1MINUTE, "1 day ago UTC")
 data_income_history = binance.futures_income_history()
@@ -52,13 +61,28 @@ df_chart["Close"] = df_chart["Close"].astype(float)
 #ax2 = ax1.twiny()
 #mpf.plot(df_chart, ax=ax2, type="candle", mav=(7, 25, 99))
 #fig.suptitle('test')
-#mpf.show()  #or plt.show()
+#mpf.show()
 
 # ローソク
-mpf.plot(df_chart, type='candle', mav=(7, 25, 99))
+#mpf.plot(df_chart, type='candle', mav=(7, 25, 99))
 
-#df_income_history.plot(x="time", y="income")
-#df_income_history.plot(figsize=(9, 6))
-plt.show()
 
-#print(df_chart)
+
+# historyからbuy,short加工
+dt_now = datetime.date.today()
+df_incom_test = pd.DataFrame(binance.futures_account_trades())
+for num in range(len(df_incom_test)):
+    df_incom_test["time"][num] = pd.to_datetime(datetime.datetime.fromtimestamp(df_incom_test["time"][num]/1000))
+
+    pd.concat([df_income_long_history, df_incom_test.iloc(num)])
+
+#    if df_incom_test["time"][0] > dt_now and df_incom_test["side"][0] == "BUY":
+        #df_income_long_history = df_incom_test.iloc(num)
+
+print(df_income_long_history)
+
+# history data
+#plt.scatter(df_income_long_history["time"], df_income_long_history["price"], s=20, c="pink", alpha=0.5, linewidths=2)
+#plt.grid("True")
+#plt.savefig("avgAnalysis.png")
+#plt.show()
