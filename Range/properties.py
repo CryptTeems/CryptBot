36f5a8655a4fbf58bfe607cf28ac1f5940efc7ea
satoakichika <m231031a@gmailcom>
2022-08-27 19:02:45 +0900

    make Master

--- Range/properties.py
+++ Range/properties.py
@@ -1,13 +1,5 @@
-from pydantic import BaseSettings
-
-class Settings(BaseSettings):
-    API_KEY: str
-    SECRET_KEY: str
-
-Settings = Settings()
-
-API_KEY = Settings.API_KEY
-SECRET_KEY = Settings.SECRET_KEY
+#API_KEY = '7MlQofOjfeh6l4lxtGaqLqr04IAxwimmuqYobvozpBmqipd259dZxxHs1tBVu70a'
+#SECRET_KEY = 'eIIwHSRH6bpKAlAmAfhVDMjt1pmRopvSp54uC9fVF09PnYmCwS4ye2rwpsfsWOa3'
 ticker = 'MATIC'  # 取引対象
 currency = 'USDT'  # USDT建て
 symbol = ticker + currency
