c4ee53b0203d2c949470e6af02846cb1a4732a1d
akck-sato <63486483+akck-sato@users.noreply.github.com>
2022-08-27 15:56:54 +0900

    Update properties.py

--- Range/properties.py
+++ Range/properties.py
@@ -1,5 +1,13 @@
-API_KEY = '7MlQofOjfeh6l4lxtGaqLqr04IAxwimmuqYobvozpBmqipd259dZxxHs1tBVu70a'
-SECRET_KEY = 'eIIwHSRH6bpKAlAmAfhVDMjt1pmRopvSp54uC9fVF09PnYmCwS4ye2rwpsfsWOa3'
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    API_KEY: str
+    SECRET_KEY: str
+
+Settings = Settings()
+
+API_KEY = Settings.API_KEY
+SECRET_KEY = Settings.SECRET_KEY
 ticker = 'MATIC'  # 取引対象
 currency = 'USDT'  # USDT建て
 symbol = ticker + currency
